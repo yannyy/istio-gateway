de55b6c2d632dec682ad8590d31620eae09576e0
yan yang <yan.yang0829@gmail.com>
2020-10-15 14:41:21 +0800

    SayHello

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/yannyy/istio-gateway
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/yannyy/istio-user v0.0.0-20201015062536-06db083d5b21
+	google.golang.org/grpc v1.32.0
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	userPb "github.com/yannyy/istio-user/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:8085"
+	defaultName = "world"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/hello/:name", sayHello)
+	r.Run()
+}
+
+func sayHello(ctx *gin.Context) {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	name := ctx.Param("name")
+
+	c := userPb.NewUserClient(conn)
+	ctx1, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	r, err := c.SayHello(ctx1, &userPb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	greeting := r.GetGreeting()
+	ctx.JSON(200, gin.H{
+		"message": greeting,
+	})
+}
